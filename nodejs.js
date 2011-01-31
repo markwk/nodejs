b585fdb94b075c799bfad4d3c49e3f29cdceb75f
Justin Randell <justinrandell@38580.no-reply.drupal.org>
2011-01-31 06:15:18 +0000

    implement a hook so modules can define their own js callbacks

--- nodejs.js
+++ nodejs.js
@@ -1,14 +1,25 @@
+
+
+
 (function ($) {
 
-Drupal.behaviors.batch = {
+Drupal.Nodejs = {'callbacks': {}};
+
+Drupal.Nodejs.runCallbacks = function (message) {
+	$.each(Drupal.Nodejs.callbacks, function () {
+		if ($.isFunction(this.callback)) {
+			this.callback(message);
+		}
+	});
+};
+
+Drupal.behaviors.nodejs = {
   attach: function (context, settings) {
 		var socket = new io.Socket(Drupal.settings.nodejs.host, {port: Drupal.settings.nodejs.port, resource: Drupal.settings.nodejs.resource});
 		socket.connect();
 		socket.send('authkey=' + Drupal.settings.nodejs.authkey);
-		socket.on('message', function(obj) {
-			if (obj.message) {
-				$('#nodejs-wrapper').append('<p>' + obj.message + '</p>');
-			}
+		socket.on('message', function(message) {
+      Drupal.Nodejs.runCallbacks(message);
 		});
   }
 };
