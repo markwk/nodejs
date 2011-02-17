5e829bc8eca81887d8bb6d8aad2335308d93257f
Justin Randell <justinrandell@38580.no-reply.drupal.org>
2011-02-17 02:36:07 +0000

    add user channel support on the node.js side. still pre-alpha

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var http = require('http'),
 		authenticatedClients = {},
     send404,
     publishMessage,
+    publishMessageToChannel,
     socket;
 
 try {
@@ -25,10 +26,33 @@ catch (e) {
 publishMessage = function (request, response) {
 	request.setEncoding('utf8');
 	request.on('data', function (chunk) {
-    socket.broadcast(chunk);
+    var publish_message = JSON.parse(chunk);
+    if (publish_message.broadcast) {
+			console.log('broadcasting to ' + publish_message.channel);
+			socket.broadcast(chunk);
+		}
+		else {
+      publishMessageToChannel(publish_message, chunk);
+		}
 	});
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end();
+}
+
+publishMessageToChannel = function (jsonObject, jsonString) {
+	if (socket.channels[jsonObject.channel]) {
+		console.log('sending to channel ' + jsonObject.channel);
+    for (var sessionId in socket.clients) {
+			console.log('looking for session ' + sessionId);
+			if (sessionId in socket.channels[jsonObject.channel]) {
+				socket.clients[sessionId].send(jsonString);
+				console.log('found session ' + sessionId + ' sending!!!');
+			}
+		}
+	}
+	else {
+		console.log('no channel to send to: ' + jsonObject.channel);
+	}
 }
 
 send404 = function(response){
@@ -46,41 +70,55 @@ server = http.createServer(function (request, response) {
   }
 });
 
-server.listen(8080);
+server.listen(drupalSettings.port, drupalSettings.host);
 
 socket = io.listen(server, {port: drupalSettings.port, resource: drupalSettings.resource});
+socket.channels = {};
 
 socket.on('connection', function(client) {
   client.on('message', function(message) {
-    var match = null, authkey = '';
-    if (match = message.match(/^authkey=(.*)$/)) {
-			authkey = match[1];
-      if (authenticatedClients[authkey]) {
-				console.log('reusing existing authkey' + authkey);
-				return;
+    message = JSON.parse(message);
+    console.log('authkey: ' + message.authkey);
+		if (authenticatedClients[message.authkey]) {
+			console.log('reusing existing authkey: ' + message.authkey);
+      for (var i = 0; i < message.channels.length; i++) {
+				console.log("adding channels for uid " + message.uid + ' ' + message.channels[i]);
+        socket.channels[message.channels[i]] = socket.channels[message.channels[i]] || {};
+        socket.channels[message.channels[i]][client.sessionId] = client.sessionId;
 			}
-			var options = {port: drupalSettings.backend.port, host: drupalSettings.backend.host, path: drupalSettings.backend.authPath + authkey};
-      http.get(options, function (response) {
-				console.log('client auth key: ' + authkey);
-        response.setEncoding('utf8');
-				response.on('data', function (chunk) {
-          var auth_data = JSON.parse(chunk);
-          if (auth_data.nodejs_valid_auth_key) {
-            console.log("got valid login for uid " + auth_data.uid);
-						authenticatedClients[authkey] = client;
+			return;
+		}
+		var options = {
+      port: drupalSettings.backend.port,
+      host: drupalSettings.backend.host,
+      path: drupalSettings.backend.authPath + message.authkey
+    };
+		http.get(options, function (response) {
+			response.setEncoding('utf8');
+			response.on('data', function (chunk) {
+				var auth_data = JSON.parse(chunk);
+				if (auth_data.nodejs_valid_auth_key) {
+					console.log("got valid login for uid " + auth_data.uid);
+					authenticatedClients[message.authkey] = true;
+					for (var i = 0; i < message.channels.length; i++) {
+						console.log("adding channels for uid " + auth_data.uid + ' ' + message.channels[i]);
+						socket.channels[message.channels[i]] = socket.channels[message.channels[i]] || {};
+						socket.channels[message.channels[i]][client.sessionId] = client.sessionId;
 					}
-					else {
-            console.log("got invalid login for uid " + auth_data.uid);
-						delete authenticatedClients[authkey];
-					}
-				});
-			}).on('error', function(e) {
-				console.log("Got error: " + e.message);
+				}
+				else {
+					console.log("got invalid login for uid " + auth_data.uid);
+					delete authenticatedClients[message.authkey];
+				}
 			});
-    }
+		}).on('error', function(e) {
+			console.log("Got error: " + e.message);
+		});
   });
   client.on('disconnect', function() {
     console.log('disconnect from client ' + client.sessionId);
   });
+}).on('error', function(client) {
+  console.log('error: ' . client);
 });
 
