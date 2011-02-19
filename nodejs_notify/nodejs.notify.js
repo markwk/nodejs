38468882ffcc163af6c01c60dd7fa3a551164839
Justin Randell <justinrandell@38580.no-reply.drupal.org>
2011-02-19 11:49:59 +0000

    call in to the new jgrowl code

--- nodejs_notify/nodejs.notify.js
+++ nodejs_notify/nodejs.notify.js
@@ -1,13 +1,11 @@
 
 (function ($) {
 
-Drupal.settings.nodejsNotify = Drupal.settings.nodejsNotify || {'dialogDomId': 'nodejs-dialog'};
-
 Drupal.Nodejs.callbacks.nodejsNotify = {
   callback: function (message) {
     switch (message.channel) {
       case 'nodejs_notify':
-        $('<div id="' + Drupal.settings.nodejsNotify.dialogDomId + '">' + message.data.subject + '</div>').dialog();
+        $.jGrowl(message.data.body);
       break;
     }
   }
