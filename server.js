2d6568670c277ba1e599111752d2221336f9c3ab
Justin Randell <justinrandell@38580.no-reply.drupal.org>
2011-02-24 04:05:34 +0000

    rearrange some code, stub out some stats callbacks, fix whitespace. so, a dogs' breakfast commit before i hop on a plane, almost certainly more brokener than it was yesterday.

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
  * It will eat your lunch and kill your kittens.
  */
 var http = require('http'),
-		url = require('url'),
+    url = require('url'),
     fs = require('fs'),
-		express = require('express'),
-		io = require(__dirname + '/socket_io/lib/socket.io'),
-		sys = require(process.binding('natives').util ? 'util' : 'sys'),
+    express = require('express'),
+    io = require(__dirname + '/socket_io/lib/socket.io'),
+    sys = require(process.binding('natives').util ? 'util' : 'sys'),
     vm = require('vm'),
     server,
-		authenticatedClients = {},
+    authenticatedClients = {},
     send404,
     publishMessage,
     publishMessageToChannel,
@@ -25,51 +25,143 @@ catch (e) {
 }
 
 publishMessage = function (request, response) {
-	request.setEncoding('utf8');
-	request.on('data', function (chunk) {
+  request.setEncoding('utf8');
+  request.on('data', function (chunk) {
     var publish_message = JSON.parse(chunk);
     if (publish_message.broadcast) {
-			console.log('broadcasting to ' + publish_message.channel);
-			socket.broadcast(chunk);
-		}
-		else {
+      console.log('broadcasting to ' + publish_message.channel);
+      socket.broadcast(chunk);
+    }
+    else {
       publishMessageToChannel(publish_message, chunk);
-		}
-	});
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end();
+    }
+  });
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end();
 }
 
 publishMessageToChannel = function (jsonObject, jsonString) {
-	if (socket.channels[jsonObject.channel]) {
-		console.log('sending to channel ' + jsonObject.channel);
+  if (socket.channels[jsonObject.channel]) {
+    console.log('sending to channel ' + jsonObject.channel);
     for (var sessionId in socket.clients) {
-			console.log('looking for session ' + sessionId);
-			if (sessionId in socket.channels[jsonObject.channel]) {
-				socket.clients[sessionId].send(jsonString);
-				console.log('found session ' + sessionId + ' sending!!!');
-			}
-		}
-	}
-	else {
-		console.log('no channel to send to: ' + jsonObject.channel);
-	}
+      console.log('looking for session ' + sessionId);
+      if (sessionId in socket.channels[jsonObject.channel]) {
+        socket.clients[sessionId].send(jsonString);
+        console.log('found session ' + sessionId + ' sending message');
+      }
+    }
+  }
+  else {
+    console.log('no channel to send to: ' + jsonObject.channel);
+  }
 }
 
-send404 = function(response){
-  response.writeHead(404);
-  response.write('404');
+/**
+ * Sends a 404 message.
+ */
+send404 = function(response) {
+  response.send('Not Found.', 404);
   response.end();
 };
 
-server = express.createServer(function (request, response) {
-  var path = url.parse(request.url).pathname;
-  switch (path) {
-    case drupalSettings.publishUrl:
-      publishMessage(request, response);
-      break;
+/**
+ * Kicks the given logged in user from the server.
+ */
+kickUser = function(request, response) {
+  if (request.params.userId) {
+    response.send({'status': 'success'});
   }
-});
+  else {
+    response.send({'status': 'failed', 'error': 'Unknown user'});
+  }
+  response.end();
+};
+
+/**
+ * Kicks the client matched to the given sessionId from the server.
+ */
+kickAnonUser = function(request, response) {
+  if (request.params.sessionId) {
+    response.send({'status': 'success'});
+  }
+  else {
+    response.send({'status': 'failed', 'error': 'Unknown session'});
+  }
+  response.end();
+};
+
+/**
+ * Bans the given user from the server.
+ */
+banUser = function(request, response) {
+  response.send({'status': 'success'});
+  response.end();
+};
+
+/**
+ * Return a summary of all channels, or more details about a single channel.
+ */
+returnChannelStats = function(request, response) {
+  var stats = {
+    'userCount': 53,
+    'clientCount': 53,
+    'created': '',
+  };
+  response.send(stats);
+  response.end();
+};
+
+/**
+ * Return summary info about the server.
+ */
+returnServerStats = function(request, response) {
+  var stats = {
+    'userCount': 53,
+    'clientCount': 53,
+    'channelCount': 53,
+    'uptime': '',
+    'memory': ''
+  };
+  response.send(stats);
+  response.end();
+};
+
+/**
+ * Return a summary of all users, or more details about a single user.
+ */
+returnUserStats = function(request, response) {
+  var stats = {};
+  if (request.params.userId) {
+    stats.user = {
+      'id': '',
+      'channels': [],
+      'lastAuthCheck': '',
+      'sessionIds': [],
+      'lastKickTime': '',
+      'banned': ''
+    };
+  }
+  else {
+    stats.users = {
+      'count': 53,
+      'ids': []
+    };
+  }
+  response.send(stats);
+};
+
+drupalSettings.serverStatsUrl = '/nodejs/stats/server';
+drupalSettings.userStatsUrl = '/nodejs/stats/user/:sessionId?';
+drupalSettings.channelStatsUrl = '/nodejs/stats/channel/:channelName?';
+drupalSettings.kickUserUrl = '/nodejs/user/kick/:sessionId';
+
+server = express.createServer();
+server.get(drupalSettings.publishUrl, publishMessage);
+server.get(drupalSettings.serverStatsUrl, returnServerStats);
+server.get(drupalSettings.userStatsUrl, returnUserStats);
+server.get(drupalSettings.channelStatsUrl, returnChannelStats);
+server.get(drupalSettings.kickUserUrl, kickUser);
+server.get('*', send404);
 
 server.listen(drupalSettings.port, drupalSettings.host);
 
@@ -80,41 +172,41 @@ socket.on('connection', function(client) {
   client.on('message', function(message) {
     message = JSON.parse(message);
     console.log('authkey: ' + message.authkey);
-		if (authenticatedClients[message.authkey]) {
-			console.log('reusing existing authkey: ' + message.authkey);
+    if (authenticatedClients[message.authkey]) {
+      console.log('reusing existing authkey: ' + message.authkey);
       for (var i = 0; i < message.channels.length; i++) {
-				console.log("adding channels for uid " + message.uid + ' ' + message.channels[i]);
+        console.log("adding channels for uid " + message.uid + ' ' + message.channels[i]);
         socket.channels[message.channels[i]] = socket.channels[message.channels[i]] || {};
         socket.channels[message.channels[i]][client.sessionId] = client.sessionId;
-			}
-			return;
-		}
-		var options = {
+      }
+      return;
+    }
+    var options = {
       port: drupalSettings.backend.port,
       host: drupalSettings.backend.host,
       path: drupalSettings.backend.authPath + message.authkey
     };
-		http.get(options, function (response) {
-			response.setEncoding('utf8');
-			response.on('data', function (chunk) {
-				var auth_data = JSON.parse(chunk);
-				if (auth_data.nodejs_valid_auth_key) {
-					console.log("got valid login for uid " + auth_data.uid);
-					authenticatedClients[message.authkey] = true;
-					for (var i = 0; i < message.channels.length; i++) {
-						console.log("adding channels for uid " + auth_data.uid + ' ' + message.channels[i]);
-						socket.channels[message.channels[i]] = socket.channels[message.channels[i]] || {};
-						socket.channels[message.channels[i]][client.sessionId] = client.sessionId;
-					}
-				}
-				else {
-					console.log("got invalid login for uid " + auth_data.uid);
-					delete authenticatedClients[message.authkey];
-				}
-			});
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
+    http.get(options, function (response) {
+      response.setEncoding('utf8');
+      response.on('data', function (chunk) {
+        var auth_data = JSON.parse(chunk);
+        if (auth_data.nodejs_valid_auth_key) {
+          console.log("got valid login for uid " + auth_data.uid);
+          authenticatedClients[message.authkey] = auth_data.uid;
+          for (var i = 0; i < message.channels.length; i++) {
+            console.log("adding channels for uid " + auth_data.uid + ' ' + message.channels[i]);
+            socket.channels[message.channels[i]] = socket.channels[message.channels[i]] || {};
+            socket.channels[message.channels[i]][client.sessionId] = client.sessionId;
+          }
+        }
+        else {
+          console.log("got invalid login for uid " + auth_data.uid);
+          delete authenticatedClients[message.authkey];
+        }
+      });
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
   });
   client.on('disconnect', function() {
     console.log('disconnect from client ' + client.sessionId);
