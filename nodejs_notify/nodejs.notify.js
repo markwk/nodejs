da54bf7c11d1e4e134eebb86b6cd3ebd6fb80b47
Justin Randell <justin.randell@gmail.com>
2011-03-02 16:11:29 -0500

    commit other change for jgrowl js

--- nodejs_notify/nodejs.notify.js
+++ nodejs_notify/nodejs.notify.js
@@ -5,9 +5,15 @@ Drupal.Nodejs.callbacks.nodejsNotify = {
   callback: function (message) {
     switch (message.channel) {
       case 'nodejs_notify':
-        $.jGrowl(message.data.body);
+        $.jGrowl(message.data.body, {header: message.data.subject});
       break;
     }
+  }
+};
+
+Drupal.Nodejs.userCallbacks.nodejsNotify = {
+  callback: function (message) {
+    $.jGrowl(message.data.body, {header: message.data.subject});
   }
 };
 
