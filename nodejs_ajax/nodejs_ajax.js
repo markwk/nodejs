29b0070031de1f39d7441bdf58653203be146dad
Michael Prasuhn <mike@mikeyp.net>
2011-03-07 00:13:05 -0800

    Cleanup nodejs_ajax callbacks, make explicit callback specification work.

--- nodejs_ajax/nodejs_ajax.js
+++ nodejs_ajax/nodejs_ajax.js
@@ -8,20 +8,30 @@
   var element = $('');
   Drupal.nodejs_ajax = new Drupal.ajax('nodejs_ajax', element, element_settings);
 
-  Drupal.Nodejs.callbacks.nodejsNodeAjax = {
+  Drupal.Nodejs.callbacks.nodejsNodeAjaxBroadcast = {
     callback: function (message) {
       switch (message.channel) {
         case 'nodejs_ajax_broadcast':
-          var response = message.commands;
-          for (var i in response) {
-            if (response[i]['command'] && Drupal.nodejs_ajax.commands[response[i]['command']]) {
-              Drupal.nodejs_ajax.commands[response[i]['command']](Drupal.nodejs_ajax, response[i], 200);
-            }
-          }
+          Drupal.nodejs_ajax.runCommands(message);
           break;
       }
     }
   };
+
+  Drupal.Nodejs.callbacks.nodejsNodeAjax = {
+    callback: function (message) {
+      Drupal.nodejs_ajax.runCommands(message);
+    }
+  };
+
+  Drupal.nodejs_ajax.runCommands = function(message) {
+    var response = message.commands;
+    for (var i in response) {
+      if (response[i]['command'] && Drupal.nodejs_ajax.commands[response[i]['command']]) {
+        Drupal.nodejs_ajax.commands[response[i]['command']](Drupal.nodejs_ajax, response[i], 200);
+      }
+    }
+  }
 
 })(jQuery);
 
