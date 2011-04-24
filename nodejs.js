efab2afaa89d5962a4fab6120a6d25fd6b17eba9
Justin Randell <justin.randell@gmail.com>
2011-04-24 13:08:09 +1000

    refactor nodejs.js

--- nodejs.js
+++ nodejs.js
@@ -3,6 +3,17 @@
 
 Drupal.Nodejs = Drupal.Nodejs || {'callbacks': {}, 'socket': false, 'connectionSetupHandlers': {}};
 
+Drupal.behaviors.nodejs = {
+  attach: function (context, settings) {
+    window.WEB_SOCKET_SWF_LOCATION = Drupal.settings.nodejs.websocketSwfLocation;
+    if (!Drupal.Nodejs.socket) {
+      if (Drupal.Nodejs.connect()) {
+        Drupal.Nodejs.sendAuthMessage();
+      }
+    }
+  }
+};
+
 Drupal.Nodejs.runCallbacks = function (message) {
   if (message.callback && $.isFunction(Drupal.Nodejs.callbacks[message.callback].callback)) {
     try {
@@ -38,45 +49,44 @@ Drupal.Nodejs.runSetupHandlers = function (type, error) {
   });
 };
 
-Drupal.behaviors.nodejs = {
-  attach: function (context, settings) {
-    if (!Drupal.Nodejs.socket) {
-      window.WEB_SOCKET_SWF_LOCATION = Drupal.settings.nodejs.websocketSwfLocation;
-      Drupal.Nodejs.socket = new io.Socket(
-        Drupal.settings.nodejs.host,
-        {secure: Drupal.settings.nodejs.secure, port: Drupal.settings.nodejs.port, resource: Drupal.settings.nodejs.resource}
-      );
-
-      Drupal.Nodejs.socket.on('message', function(newMessage) {
-        Drupal.Nodejs.runCallbacks(newMessage);
-      });
-
-      try {
-        Drupal.Nodejs.socket.connect();
-        Drupal.Nodejs.runSetupHandlers('connectionSuccess');
-      }
-      catch (exception) {
-        Drupal.Nodejs.socket = false;
-        Drupal.Nodejs.runSetupHandlers('connectionFailure', exception);
-        return;
-      }
-
-      try {
-        var authMessage = {
-          type: 'authenticate',
-          authkey: Drupal.settings.nodejs.authkey
-        };
-        Drupal.Nodejs.socket.send(authMessage);
-        Drupal.Nodejs.runSetupHandlers('authSuccess');
-      }
-      catch (exception) {
-        Drupal.Nodejs.socket = false;
-        Drupal.Nodejs.runSetupHandlers('authFailure', exception);
-        return;
-      }
+Drupal.Nodejs.connect = function () {
+  try {
+    var socketSettings = {
+      secure: Drupal.settings.nodejs.secure, 
+      port: Drupal.settings.nodejs.port, 
+      resource: Drupal.settings.nodejs.resource
     }
+    Drupal.Nodejs.socket = new io.Socket(Drupal.settings.nodejs.host, socketSettings);
+    Drupal.Nodejs.socket.connect();
+    Drupal.Nodejs.runSetupHandlers('connectionSuccess');
+    Drupal.Nodejs.socket.on('message', Drupal.Nodejs.runCallbacks);
+    return true;
+  }
+  catch (exception) {
+    Drupal.Nodejs.socket = false;
+    Drupal.Nodejs.runSetupHandlers('connectionFailure', exception);
+    return false;
+  }
+};
+
+Drupal.Nodejs.sendAuthMessage = function () {
+  try {
+    var authMessage = {
+      type: 'authenticate',
+      authkey: Drupal.settings.nodejs.authkey
+    };
+    Drupal.Nodejs.socket.send(authMessage);
+    Drupal.Nodejs.runSetupHandlers('authSuccess');
+    return true;
+  }
+  catch (exception) {
+    Drupal.Nodejs.socket = false;
+    Drupal.Nodejs.runSetupHandlers('authFailure', exception);
+    return false;
   }
 };
 
 })(jQuery);
+
+// vi:ai:expandtab:sw=2 ts=2
 
