38a7095f6165f229f479eed074f0fb9c050fbc10
John Robert Wilson <johnrobert@gmail.com>
2011-04-26 17:29:40 -0400

    Issue #1138250 by LSU_JBob: Extend Node.js Notifications: Config Settings & System-wide Broadcast

--- nodejs_notify/nodejs.notify.js
+++ nodejs_notify/nodejs.notify.js
@@ -3,7 +3,7 @@
 
 Drupal.Nodejs.callbacks.nodejsNotify = {
   callback: function (message) {
-    $.jGrowl(message.data.body, {header: message.data.subject});
+    $.jGrowl(message.data.body, {header: message.data.subject, life:(Drupal.settings.nodejs_notify.notification_time*1000)});
   }
 };
 
