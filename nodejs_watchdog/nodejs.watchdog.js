daee3ebf6326578b904baf91be77c59a75980aac
Justin Randell <justin.randell@gmail.com>
2011-06-18 14:50:51 +1000

    #1190020 by msonnabaum: adding nodejs_watchdog module

---
+++ nodejs_watchdog/nodejs.watchdog.js
@@ -0,0 +1,8 @@
+(function ($) {
+  Drupal.behaviors.nodejsWatchdog = {
+    attach: function () {
+      $("#admin-dblog tr:even").removeClass('odd').addClass('even');
+      $("#admin-dblog tr:odd").removeClass('even').addClass('odd');
+    }
+  };
+}(jQuery));
