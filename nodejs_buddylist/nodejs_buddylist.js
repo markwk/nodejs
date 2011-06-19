b0fc46b10af7fe393b22f75a1e91c85e573d9318
John Robert Wilson <johnrobert@gmail.com>
2011-06-19 10:36:45 -0400

    Node.js Buddylist module initial commit born from NYC Drupal Playday 6-18-2011

---
+++ nodejs_buddylist/nodejs_buddylist.js
@@ -0,0 +1,10 @@
+(function ($) {
+
+Drupal.Nodejs.callbacks.nodejsBuddylist.refreshList = {
+  callback: function (container_id, data) {
+    //jquery that refreshes the selector for buddy list, or user list
+    //Maybe a simple jquery animation?
+  }
+};
+
+})(jQuery);
