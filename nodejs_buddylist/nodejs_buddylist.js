5e1b648a04badb421be0ce4acc6bb52d224d6505
Justin Randell <justin.randell@gmail.com>
2011-08-24 08:32:41 +1000

    big dump

--- nodejs_buddylist/nodejs_buddylist.js
+++ nodejs_buddylist/nodejs_buddylist.js
@@ -13,6 +13,32 @@ Drupal.Nodejs.presenceCallbacks.buddyList = {
   }
 };
 
+Drupal.Nodejs.callbacks.nodejsBuddyListStartChat = {
+  callback: function (message) {
+    alert(message);
+  }
+};
+
+/**
+ * Add behaviours to buddylist elements.
+ */
+Drupal.behaviors.buddyList = {
+  attach: function (context, settings) {
+    $('.nodejs-buddylist-start-chat-link').click(function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      var matches = this.href.match(/start-chat-(\d+)/);
+      $.ajax({
+        type: 'POST',
+        url: Drupal.settings.basePath + 'nodejs-buddylist/start-chat',
+        dataType: 'json',
+        success: function () {},
+        data: {uid: matches[1]}
+      });
+    });
+  }
+};
+
 })(jQuery);
 
 // vi:ai:expandtab:sw=2 ts=2
