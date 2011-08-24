200b34591eab12401066eef9032c973d89f6b4d9
= <=>
2011-08-24 08:27:53 +0100

    Touchups to display and hide buddy list

--- nodejs_buddylist/nodejs_buddylist.js
+++ nodejs_buddylist/nodejs_buddylist.js
@@ -5,10 +5,14 @@ Drupal.Nodejs.presenceCallbacks.buddyList = {
     if (message.presenceNotification.event == 'offline') {
       $('#nodejs-buddylist-uid-' + message.presenceNotification.uid).removeClass('nodejs-buddylist-online');
       $('#nodejs-buddylist-uid-' + message.presenceNotification.uid).addClass('nodejs-buddylist-offline');
+      $('#nodejs-chatbar-uid-' + message.presenceNotification.uid).removeClass('nodejs-buddylist-online');
+      $('#nodejs-chatbar-uid-' + message.presenceNotification.uid).addClass('nodejs-buddylist-offline');
     }
     else {
       $('#nodejs-buddylist-uid-' + message.presenceNotification.uid).addClass('nodejs-buddylist-online');
       $('#nodejs-buddylist-uid-' + message.presenceNotification.uid).removeClass('nodejs-buddylist-offline');
+      $('#nodejs-chatbar-uid-' + message.presenceNotification.uid).addClass('nodejs-buddylist-online');
+      $('#nodejs-chatbar-uid-' + message.presenceNotification.uid).removeClass('nodejs-buddylist-offline');
     }
   }
 };
@@ -26,7 +30,20 @@ Drupal.behaviors.buddyList = {
   attach: function (context, settings) {
     $('body').append(Drupal.settings.chatbar_settings);
     $('#chatbar .tab-button').click(function () {
-      $(this).siblings('.chatbar-pane').slideToggle(100);
+      var sibling_pane = $(this).siblings('.chatbar-pane');
+      var container = $(this).parent();
+
+      if (container.width() > sibling_pane.width()) {
+        sibling_pane.width(container.width());
+      }
+      else {
+        container.width(sibling_pane.width());
+      }
+      sibling_pane.slideToggle(100, function() {
+        if ($(this).css('display') == 'none') {
+          container.width('auto');
+        }
+      });
     });
     $('.nodejs-buddylist-start-chat-link').click(function (e) {
       e.preventDefault();
